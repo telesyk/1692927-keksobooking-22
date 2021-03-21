0576c9881761dbfb8c4cdcfc59c31e7c82ec204d
telesyk <oles.tkachuk@yola.com>
2021-03-21 16:05:32 +0200

    Fix path to 'advert-form'.

--- js/modules/index.js
+++ js/modules/index.js
@@ -1,6 +1,6 @@
 // import adverts from './adverts.js';
 import { adverts } from './adverts/index.js';
 import card from './card/index.js';
-import advertForm from './advertForm/index.js';
+import advertForm from './advert-form/index.js';
 
 export default { advertForm, card, adverts };
